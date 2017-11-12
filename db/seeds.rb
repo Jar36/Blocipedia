0ced097d7fef59a5ba89576971a49183cdebc618
Joseph Romero <jromero361980@gmail.com>
2017-11-12 22:59:51 +0000

    help-2-seeding-WIP

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,27 @@
 require 'random_data'
+
+ # Create an admin user
+ admin = User.create!(
+   email:    'admin@example.com',
+ )
+
+# Create a member
+ member = User.create!(
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+
+users = Users.all
+
  # Create Wikis
  10.times do
    Wiki.create!(
      title:  RandomData.random_sentence,
-     body:   RandomData.random_paragraph
+     body:   RandomData.random_paragraph,
+     users: users.sample
    )
  end
- wikis = Wiki.all
-  # Create an admin user
- admin = User.create!(
-   name:     'Admin User',
-   email:    'admin@example.com',
-   password: 'helloworld',
-   role:     'admin'
- )
- 
- # Create a member
- member = User.create!(
-   name:     'Member User',
-   email:    'member@example.com',
-   password: 'helloworld'
- )
+
  
  puts "#{User.count} users created"
  puts "Seed finished"
