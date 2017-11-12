2a656d6d694c1f9e8324e51ada13ae2286214fb8
Joseph Romero <jromero361980@gmail.com>
2017-11-12 20:55:41 +0000

    add seeds and a wikis controller-WIP

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,21 @@
 Rails.application.routes.draw do
+  # get 'wikis/index'
+
+  # get 'wikis/show'
+
+  # get 'wikis/new'
+
+  # get 'wikis/edit'
+  
+  resources :wikis
+
   devise_for :users
+ 
   root 'welcome#index'
   
-  get 'welcome/index'
+  # get 'welcome/index'
 
-  get 'welcome/about'
+  get 'about' => 'welcome/about'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,27 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'random_data'
+ # Create Wikis
+ 10.times do
+   Wiki.create!(
+     title:  RandomData.random_sentence,
+     body:   RandomData.random_paragraph
+   )
+ end
+ wikis = Wiki.all
+  # Create an admin user
+ admin = User.create!(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ 
+ # Create a member
+ member = User.create!(
+   name:     'Member User',
+   email:    'member@example.com',
+   password: 'helloworld'
+ )
+ 
+ puts "#{User.count} users created"
+ puts "Seed finished"
+ puts "#{Wiki.count} wikis created"
