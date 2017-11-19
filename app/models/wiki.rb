29af9d49094f2b3ea71a64964d3d1b1da2fde342
Joseph Romero <jromero361980@gmail.com>
2017-11-19 16:44:59 +0000

    add wiki scope order

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,4 +1,6 @@
 class Wiki < ApplicationRecord
   belongs_to :user
-  
+  default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { order('created_at ASC') }
+  scope :ordered_by_reverse_created_at, -> { order('created_at ASC') }
 end
